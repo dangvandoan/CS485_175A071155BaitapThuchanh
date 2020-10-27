699b6da44fdd2a410d773d6ac8e0810836eb5e43
dangvandoan <DoanDV72@wru.vn>
2020-10-27 09:54:09 +0700

    BTTH1

---
+++ 1.BTTH/BTTH1/index.php
@@ -0,0 +1,135 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <title>Title</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+            <!-- Font awesone-->
+    <link href="fontawesome-free-5.14.0-web/css/all.min.css" rel="stylesheet">
+    <!--Google font -->
+    <link href="https://fonts.googleapis.com/css2?family=Candal&family=Lora&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+    <header>
+        <nav class="navbar navbar-expand-sm  ">
+            <h1 class="logo-text"><span>Awa</span>Inspires</h1>
+            <button class="navbar-toggler d-lg-none" type="button" data-toggle="collapse"
+                data-target="#collapsibleNavId" aria-controls="collapsibleNavId" aria-expanded="false"
+                aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="nav">
+                <ul class="navbar-nav ml-auto mt-4 mt-lg-0">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">About</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Services</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Sign Up</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Login</a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="dropdownId" data-toggle="dropdown"
+                            aria-haspopup="true" aria-expanded="false">Awa Melvine</a>
+                        <div class="dropdown-menu" aria-labelledby="dropdownId">
+                            <a class="dropdown-item" href="#" style="color: black;">Dashboard </a>
+                            <a class="dropdown-item" href="#" style="color: red;">Logout</a>
+                        </div>
+                    </li>
+                </ul>
+
+            </div>
+        </nav>
+    </header>
+    <!--Page Wrapper-->
+    <div class="page-wrapper">
+        <!--Post Slider-->
+        <div class="post-slider">
+            <h1 class="slider-title">Trending Posts</h1>
+            <i class="fas fa-chevron-left prev"></i>
+            <i class="fas fa-chevron-right next"></i>
+            <div class="post-wrapper">
+
+                <div class="post">
+                    <img src="imgs/bg3.jpg" alt="" class="slider-image">
+                    <div class="post-info">
+                        <h4><a href="single.html">One day your life will flash before your eyes</a></h4>
+                        <i class="far fa-user">Awa Melvine</i> &nbsp;
+                        <i class="far fa-calendar">Mar 8, 2019</i>
+                    </div>
+                </div>
+
+                <div class="post">
+                    <img src="imgs/bg6.jpg" alt="" class="slider-image">
+                    <div class="post-info">
+                        <h4><a href="single.html">One day your life will flash before your eyes</a></h4>
+                        <i class="far fa-user">Awa Melvine</i> &nbsp;
+                        <i class="far fa-calendar">Mar 8, 2019</i>
+                    </div>
+                </div>
+
+                <div class="post">
+                    <img src="imgs/bg8.jpg" alt="" class="slider-image">
+                    <div class="post-info">
+                        <h4><a href="single.html">One day your life will flash before your eyes</a></h4>
+                        <i class="far fa-user">Awa Melvine</i> &nbsp;
+                        <i class="far fa-calendar">Mar 8, 2019</i>
+                    </div>
+                </div>
+
+                <div class="post">
+                    <img src="imgs/bg18.jpg" alt="" class="slider-image">
+                    <div class="post-info">
+                        <h4><a href="single.html">One day your life will flash before your eyes</a></h4>
+                        <i class="far fa-user">Awa Melvine</i> &nbsp;
+                        <i class="far fa-calendar">Mar 8, 2019</i>
+                    </div>
+                </div>
+
+
+                <div class="post">
+                    <img src="imgs/bg55.jpg" alt="" class="slider-image">
+                    <div class="post-info">
+                        <h4><a href="single.html">One day your life will flash before your eyes</a></h4>
+                        <i class="far fa-user">Awa Melvine</i> &nbsp;
+                        <i class="far fa-calendar">Mar 8, 2019</i>
+                    </div>
+                </div>
+
+            </div>
+
+        </div>
+        <!--// Post slider-->
+
+        <!--Content-->
+    </div>
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous">
+    </script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
+        integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous">
+    </script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
+        integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous">
+    </script>
+    <script src="js/script.js"></script>
+</body>
+
+</html>
